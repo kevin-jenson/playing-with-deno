3ed6bfb5d671804647f8973fee0dafb69250683d
Kevin Jenson <kjenson@Kevins-MacBook-Pro-2.local>
2020-05-07 23:38:33 -0600

    wrote my first deno server

---
+++ helloWorld.ts
@@ -0,0 +1,23 @@
+import { Application, Router } from 'https://deno.land/x/oak/mod.ts';
+
+let idSet: Array<number> = [];
+for (let i = 0; i < 100; i++) {
+	idSet.push(i);
+}
+
+const router = new Router();
+router.get('/', ctx => {
+	ctx.response.body = 'Hello World!';
+}).get('/ids', ctx => {
+	ctx.response.body = idSet;
+}).get('/ids/:id', ctx => {
+	ctx.response.body = idSet.find(id => ctx.params?.id)
+})
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+const PORT = 5000;
+console.log(`Listening on port 🚀 ${PORT} 🚀`);
+await app.listen({ port: PORT });
